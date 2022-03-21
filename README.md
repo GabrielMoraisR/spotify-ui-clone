8fef931554406c68711204e89d3eab09afca128e
Gabriel Morais <morais.g99@gmail.com>
2022-03-21 16:06:18 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+
+
+
+
+# Clone UI do Spotify! :musical_note:
+
+O projeto foi feito com o propósito de estudar mais sobre como funciona CSS3 e o uso do mesmo com componentização em React.js
+
+## 🚀 Começando
+
+
+## Rodando o projeto / 
+
+Para clonar o projeto, basta usar o seguinte comando:
+
+### `git clone` + o link do repositorio
+
+Depois de ter clonado o repositório, rode o seguinte comando para baixar todas as dependêcias do projeto:
+
+### `npm install`
+
+OBS: Pode ser que o projeto contenha erros dependendo da versão npm que você tem na sua máquina, para a correção de possiveis erros como esse, rode o seguinte comando: 
+
+### `npm audit fix`
+
+Pronto, agora é só rodar o projeto direto da sua máquina! :wink:
+
+Use o seguinte comando:
+
+### `npm start`
+
+```
+
+## 🛠️ Construído com
+
+Mencione as ferramentas que você usou para criar seu projeto
+
+* [ReactJS](https://pt-br.reactjs.org/) - O framework web usado
+
+## ✒️ Autores
+
+* **Gabriel de Morais** - *Trabalho Total* - [Gabriel de Morais](https://gist.github.com/GabrielMoraisR)
+
+## 🎁 Expressões de gratidão
+
+* Um projeto muito desafiador , fiquei bastante tempo desenvolvendo e fazendo melhorias , devido a demando do serviço fui fazendo as correções aos poucos. 📢
+
---
+++ src/components/Recently/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './Recently.css';
+
+export default function Recently() {
+    return (
+    <div>
+        <section className="columns">
+
+            <div className="item-column">
+                <div className="item">
+                    <div className="image-default"></div>
+                    <p>Músicas Curtidas</p>
+                </div>
+
+                <div className="item">
+                    <div className="image-default"></div>
+                    <p>Projota</p>
+                </div>
+            </div>
+
+            <div className="item-column">
+                <div className="item">
+                    <div className="image-default"></div>
+                    <p>Rap</p>
+                </div>
+
+                <div className="item">
+                    <div className="image-default"></div>
+                    <p>Mano Brown</p>
+                </div>
+            </div>
+
+            <div className="item-column">
+                <div className="item">
+                    <div className="image-default"></div>
+                    <p>Músicas Curtidas</p>
+                </div>
+
+                <div className="item">
+                    <div className="image-default"></div>
+                    <p>Músicas Curtidas</p>
+                </div>
+            </div>
+        </section>
+    </div>
+    )
+}
